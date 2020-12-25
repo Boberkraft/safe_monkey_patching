177c0b1050cff4bb2dbd5edd88affd3726de9ba3
Andrzej Bisewski <andrzej.bisewski@gmail.com>
2020-12-25 23:24:35 +0100

    Update kernel.rb

--- kernel.rb
+++ kernel.rb
@@ -1,14 +1,16 @@
 Kernel.class_exec do
   def monkey_path(method)
-    database = if File.exists?('monkey_paths.yml')
+    database = begin
                  YAML.load_file('monkey_paths.yml')
-               else
+               rescue Errno::ENOENT
                  {}
                end
-    current = { method.owner.to_s => { method.name.to_s => { 'sha1' => Digest::SHA1.hexdigest(method.source) } }
+    current = {
+      method.owner.to_s => { method.name.to_s =>
+                               { 'sha1' => Digest::SHA1.hexdigest(method.source) }
+      }
     }
     new_database = database.merge(current)
     File.open('monkey_paths.yml', 'w') { |f| f.puts(new_database.to_yaml) }
-    puts 'ok!'
   end
 end
